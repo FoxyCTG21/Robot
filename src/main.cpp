89f3420f85272f0d0530caba5f46c8fc3f15a262
FoxyCTG21 <cristiantg2014@hotmail.es>
2022-03-02 11:03:36 +0100

    feat: Espaciados

--- src/main.cpp
+++ src/main.cpp
@@ -21,80 +21,80 @@
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);          // make sure your Serial Monitor is also set at this baud rate.
-  Dabble.begin("MyFoxyRobot"); // set bluetooth name of your device
+    // put your setup code here, to run once:
+    Serial.begin(9600);          // Marcamos la velocidad de la transmisión serial
+    Dabble.begin("MyFoxyRobot"); // Indicamos un nombre al dispositivo a encontrar
 }
 
 void loop()
 {
-  Dabble.processInput(); // this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
-  Serial.print("KeyPressed: ");
-  if (GamePad.isUpPressed())
-  {
-    Serial.print("Up");
-  }
+    Dabble.processInput(); // this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
+    Serial.print("KeyPressed: ");
+    if (GamePad.isUpPressed())
+    {
+        Serial.print("Up");
+    }
 
-  if (GamePad.isDownPressed())
-  {
-    Serial.print("Down");
-  }
+    if (GamePad.isDownPressed())
+    {
+        Serial.print("Down");
+    }
 
-  if (GamePad.isLeftPressed())
-  {
-    Serial.print("Left");
-  }
+    if (GamePad.isLeftPressed())
+    {
+        Serial.print("Left");
+    }
 
-  if (GamePad.isRightPressed())
-  {
-    Serial.print("Right");
-  }
+    if (GamePad.isRightPressed())
+    {
+        Serial.print("Right");
+    }
 
-  if (GamePad.isSquarePressed())
-  {
-    Serial.print("Square");
-  }
+    if (GamePad.isSquarePressed())
+    {
+        Serial.print("Square");
+    }
 
-  if (GamePad.isCirclePressed())
-  {
-    Serial.print("Circle");
-  }
+    if (GamePad.isCirclePressed())
+    {
+        Serial.print("Circle");
+    }
 
-  if (GamePad.isCrossPressed())
-  {
-    Serial.print("Cross");
-  }
+    if (GamePad.isCrossPressed())
+    {
+        Serial.print("Cross");
+    }
 
-  if (GamePad.isTrianglePressed())
-  {
-    Serial.print("Triangle");
-  }
+    if (GamePad.isTrianglePressed())
+    {
+        Serial.print("Triangle");
+    }
 
-  if (GamePad.isStartPressed())
-  {
-    Serial.print("Start");
-  }
+    if (GamePad.isStartPressed())
+    {
+        Serial.print("Start");
+    }
 
-  if (GamePad.isSelectPressed())
-  {
-    Serial.print("Select");
-  }
-  Serial.print('\t');
+    if (GamePad.isSelectPressed())
+    {
+        Serial.print("Select");
+    }
+    Serial.print('\t');
 
-  int a = GamePad.getAngle();
-  Serial.print("Angle: ");
-  Serial.print(a);
-  Serial.print('\t');
-  int b = GamePad.getRadius();
-  Serial.print("Radius: ");
-  Serial.print(b);
-  Serial.print('\t');
-  float c = GamePad.getXaxisData();
-  Serial.print("x_axis: ");
-  Serial.print(c);
-  Serial.print('\t');
-  float d = GamePad.getYaxisData();
-  Serial.print("y_axis: ");
-  Serial.println(d);
-  Serial.println();
+    int a = GamePad.getAngle();
+    Serial.print("Angle: ");
+    Serial.print(a);
+    Serial.print('\t');
+    int b = GamePad.getRadius();
+    Serial.print("Radius: ");
+    Serial.print(b);
+    Serial.print('\t');
+    float c = GamePad.getXaxisData();
+    Serial.print("x_axis: ");
+    Serial.print(c);
+    Serial.print('\t');
+    float d = GamePad.getYaxisData();
+    Serial.print("y_axis: ");
+    Serial.println(d);
+    Serial.println();
 }
